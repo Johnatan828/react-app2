a3bc1fc412f7f0bd873556a92f6a5c00e39869c5
Johnathan Garay <Johnathan.Garay@teaminternational.com>
2019-04-26 14:44:04 -0500

    exected version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -76,19 +76,19 @@ class App extends Component {
       style.backgroundColor ='red';
      
     }
-    let classes = [];
+    let assignedClasses = [];
     if(this.state.persons.length <=2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if(this.state.persons.length <=1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
       
-      <div className="App">
-        <h1> Hi, I'm a React </h1>
-        <p className= {classes.join(' ')}> This is really Good!</p>
+      <div className={classes.App}>
+        <h1> Hi, I'm a React App</h1>
+        <p className= {assignedClasses.join(' ')}> This is really Good!</p>
         <button
         //Inline style applied here... 
           style={style}
